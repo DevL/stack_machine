1bf6e3f615f9f46302997fa4cea801b30acda9fd
Lennart Fridén <lennart@devl.se>
2016-07-11 23:54:45 +0200

    Update Elixir and deps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule StackMachine.Mixfile do
     [
       app: :stack_machine,
       version: "0.0.1",
-      elixir: "~> 1.2",
+      elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps,
@@ -36,8 +36,8 @@ defmodule StackMachine.Mixfile do
   defp deps do
     [
       {:earmark, "~> 0.2", only: :dev},
-      {:ex_doc, "~> 0.11", only: :dev},
-      {:inch_ex, ">= 0.4.0", only: :docs}
+      {:ex_doc, "~> 0.12", only: :dev},
+      {:inch_ex, ">= 0.5.3", only: :docs}
     ]
   end
 end
